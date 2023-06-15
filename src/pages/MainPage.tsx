37edb05fddc596002927d72bdfb7901bc36aeae7
Gargant <gargant0373@gmail.com>
2023-06-15 13:58:01 +0200

    added footer and some small changes

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import PostArea from "../components/PostArea";
 import Timer from "../components/Timer";
@@ -7,7 +8,12 @@ function MainPage() {
         <>
             <Navbar />
             <Timer />
-            <PostArea marginTop={"20px"}/>
+            <PostArea marginTop={"20px"} />
+            <div style={{
+                margin: "0", backgroundColor: "#1f1f1f", position: "fixed", left: "0", bottom: "0", width: "100%"
+            }}>
+                <Footer />
+            </div>
         </>
     );
 }
