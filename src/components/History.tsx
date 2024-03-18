31833f62abd4d7c0336853b7114677ad0e3527f8
Matei Pavel Dumitrescu <M.P.Dumitrescu@student.tudelft.nl>
2024-03-18 23:16:01 +0100

    mobile layout

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,62 +1,108 @@
 import { Card, CardContent, Grid } from "@mui/material";
 import "../styles/History.css";
 import { Parallax, ParallaxProvider } from "react-scroll-parallax";
+import { useState, useEffect } from 'react';
 function History() {
+    const [parallaxSpeed, setParallaxSpeed] = useState(-2);
+    const [shortText1, setShortText1] = useState(false);
+    const [shortText2, setShortText2] = useState(false);
+
+    const [showMoreTextButton, setShowMoreTextButton] = useState(false);
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 900) {
+                setParallaxSpeed(0);
+                setShowMoreTextButton(true);
+            } else {
+                setParallaxSpeed(-2);
+                setShowMoreTextButton(false);
+            }
+        }
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return <>
         <Grid container className="history" id="history">
             <ParallaxProvider>
                 <Grid item xs={12} className="ptitle">
                     <h1 className="chtitle">HISTORY</h1>
                 </Grid>
-                <Grid item xs={10} md={4}>
-                    <Parallax speed={-2}>
+                <Grid item xs={11} md={4}>
+                    <Parallax speed={parallaxSpeed}>
                         <img src="7.jpeg" alt="" />
                     </Parallax>
                 </Grid>
                 <Grid item xs={11} md={7}>
                     <Card className="card wmargin minh">
                         <CardContent>
-                            <div className="chsubtitle primary underlined">1st of May</div>
-                            <div className="chtext primary">
-                                It all kicked off on the 1st of May, 2023, a day meant for honoring workers, but in Romania, it became the birthdate of something unexpected and delightful. Picture this: around 50
-                                eager souls gathered on one side of the lake in Delftse Hout, while another 30 claimed their spot on the opposite bank. What was the occasion, you ask? A grand
-                                celebration of grilling mastery paired with abundant sunshine and an endless flow of beer. <br />
-                                As the aroma of grilling meats filled the air, it marked the beginning of something extraordinary. Through clever promotion within the TU Delft community, what began as a simple <br />
-                                barbecue transformed into "The Grill," a campus-wide sensation embodying unity amidst academia. <br />
-                                With each delicious bite and refreshing sip, The Grill embedded itself deeper into TU Delft's culture. From a modest gathering, it blossomed into a cherished tradition, fostering
-                                connections and cherished memories for students and beyond. <br />
-                                As the sun set on that memorable May day, The Grill had become an integral part of TU Delft's identity, promising joy and camaraderie for years to come.
-                            </div>
+                            <h1 className="chsubtitle primary underlined">1st of May</h1>
+                            <p className="chtext primary">
+                                It all kicked off on the 1st of May, 2023, a day meant for honoring workers, but in Romania, it became the birthday of something unexpected and delightful.
+                                <br />
+                                Picture this: around 50
+                                eager souls gathered on one side of the lake in Delftse Hout, while another 30 claimed their spot on the opposite bank.
+                                <br />
+                                What was the occasion, you ask? A grand
+                                celebration of grilling mastery paired with abundant sunshine and an endless flow of beer.
+                                <br />
+                                {!shortText1 &&
+                                    <span>
+                                        As the aroma of grilling meats filled the air, it marked the beginning of something extraordinary. Through clever promotion within the TU Delft community, what began as a simple
+                                        <br />
+                                        barbecue transformed into "The Grill," a campus-wide sensation embodying unity amidst academia. <br />
+                                        With each delicious bite and refreshing sip, The Grill embedded itself deeper into TU Delft's culture. From a modest gathering,
+                                        it blossomed into a cherished tradition, fostering
+                                        connections and cherished memories for students and beyond.
+                                        <br />
+                                        As the sun set on that memorable May day, The Grill had become an integral part of TU Delft's identity,
+                                        promising joy and camaraderie for years to come.
+                                    </span>}
+
+                            </p>
                         </CardContent>
+                        {showMoreTextButton && (<button type="button" className="showmore" 
+                        onClick={() => setShortText1(!shortText1)}> {shortText1 ? "Read more..." : "Read less..."} </button>)}
                     </Card>
                 </Grid>
                 <Grid item xs={11} md={7}>
                     <Card className="card wmargin minh">
                         <CardContent>
-                            <div className="chsubtitle primary underlined">Other Grills</div>
-                            <div className="chtext primary">
+                            <h1 className="chsubtitle primary underlined">Other Grills</h1>
+                            <p className="chtext primary">
                                 Next month, another big celebration was planned, the 1st of June, the International Children's Day. How to enjoy these day better than with a grill next to the beautiful
-                                lake Delftsehout, that took all of us streight to our childhood? All memories from when we were little and we would as girls help our moms prepare the salads, and
-                                while the boys would bring wood for their dads, and help them with cooking the meet. Everyone is <br />
-                            </div>
+                                lake Delftse Hout, that took all of us straight to our childhood?
+                                <br />
+                                {!shortText2 &&
+                                    <span> All memories from when we were little and we would as girls help our moms prepare the salads, and
+                                while the boys would bring wood for our dads and help them with cooking the meet.
+                                <br />
+                                Everyone is welcomed to join and relive those cherished moments,
+                                to blend the joy of childhood memories with the warmth of community spirit. </span>}
+                            </p>
                         </CardContent>
+                        {showMoreTextButton && (<button type="button" className="showmore" 
+                        onClick={() => setShortText2(!shortText2)}> {shortText2 ? "Read more..." : "Read less..."} </button>)}
+
                     </Card>
                 </Grid>
                 <Grid item xs={10} md={4}>
                     <img src="10.jpeg" alt="" />
                 </Grid>
                 <Grid item xs={10} md={4}>
-                    <Parallax speed={-2}>
+                    <Parallax speed={parallaxSpeed}>
                         <img src="9.jpeg" alt="" />
                     </Parallax>
                 </Grid>
                 <Grid item xs={11} md={7}>
                     <Card className="card wmargin minh">
                         <CardContent>
-                            <div className="chsubtitle primary underlined">Other Grills</div>
+                            <h1 className="chsubtitle primary underlined">Other Grills</h1>
                             <div className="chtext primary">
                                 Obviously the legendary grill was the original one. Many followed, there were many drifting cars, wooden sticks picked up, fires extinguished.<br />
-                                At the end of the day one thing is certain though- many beers, laughs and mici have been consumed.
+                                At the end of the day one thing is certain though - many beers, laughs and <span className="rogradient">mici</span>  have been consumed.
                             </div>
                         </CardContent>
                     </Card>
