0b2c849548c4e20d3bc02844cdd945708b750a08
Matei Pavel Dumitrescu <M.P.Dumitrescu@student.tudelft.nl>
2024-03-27 00:06:34 +0100

    added god rays

---
+++ src/components/GodRays.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../styles/GodRays.scss'
+import { useEffect, useState, useRef } from 'react'
+function GodRays() {
+    //make a scroll event listener that changes:
+    // the opacity of the rays (a little bit)
+    // the deg of the gradient
+    // the blur of the gradient
+    const rayDiv = useRef<HTMLDivElement>(null)
+    const rayContainer = useRef<HTMLDivElement>(null)
+    const handleScroll = () => {
+        const scroll = window.scrollY;
+        let deg = 100 + (scroll / 75);
+        rayContainer.current?.style.setProperty('--gradientDeg', `${deg}deg`);
+    }
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, [])
+    return (
+        <div  ref={rayContainer} className='raysContainer'>
+            <div ref={rayDiv} className="rayDiv"></div>
+        </div>
+    )
+}
+
+export default GodRays
