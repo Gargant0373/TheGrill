1c9716d73400282b68d7887dfdeee3af26db65df
Miruna <mirunanego15@gmail.com>
2025-03-24 14:32:25 +0100

    make lip animation using motion

--- thegrill/src/App.tsx
+++ thegrill/src/App.tsx
@@ -1,10 +1,23 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import './Mouth.css';
 import EventMap from './components/EventMap';
 import Rules from './components/Guidelines';
+import { motion, useScroll, useTransform } from "motion/react"
 
 function App() {
+
+  const { scrollY } = useScroll();
+  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+
+  // Update viewport height on resize -- I chatGPTd this :)
+  useEffect(() => {
+    const updateHeight = () => setViewportHeight(window.innerHeight);
+    window.addEventListener("resize", updateHeight);
+    return () => window.removeEventListener("resize", updateHeight);
+  }, []);
+
+  const lipOffset = useTransform(scrollY, [0, viewportHeight], [0, -2 * viewportHeight])
+  const lipOpacity = useTransform(scrollY, [0, 1.3 * viewportHeight], [1, 0])
 
   const lipThreshold = 500;
   const contentFadeStart = 500;
@@ -27,37 +40,48 @@ function App() {
 
   return (
     <>
-      <div
-        className="mouth"
-        style={{ opacity: 1 - lipProgress, transition: "opacity 0.5s ease" }}
-      >
-        <div className="upper">
-          <img
+
+      <div className="start-page" >
+        <motion.div
+          className="mouth"
+          style={{ position: 'absolute', width: '100%', height: '100vh', zIndex: 5 }}
+        >
+          <motion.img
+            className="upper"
             src="./assets/toplip.png"
             alt="Top lip of The Grill Master"
-            style={{ transform: `translateY(-${scrollOffset}px)` }}
+            style={{ width: '100%', position: 'absolute', zIndex: 5, top: lipOffset, opacity: lipOpacity }}
           />
-        </div>
-        <div className="lower">
-          <img
+          <motion.img
+            className="lower"
             src="./assets/downlip.png"
-            alt="Down lip of The Grill Master"
-            style={{ transform: `translateY(${scrollOffset}px)` }}
+            alt="Bottom lip of The Grill Master"
+            style={{ width: '100%', position: 'absolute', zIndex: 4, bottom: lipOffset, opacity: lipOpacity }}
           />
+        </motion.div>
+        <div className="info-section" style={{ zIndex: 2, position: 'absolute', width: '100%', backgroundColor: 'blue' }}>
+          <div className="roof-of-mouth" style={{ position: 'absolute', width: '100%', top: '0vh' }}>
+            <img
+              src="./assets/insidemouth.png"
+              alt="Inside of The Grill Master's mouth"
+              style={{ position: 'absolute', width: '100%' }}
+            />
+          </div>
         </div>
-      </div>
-      <div className="content">
+      </div >
+
+
+
+
+      <div className="content2">
         <img
-          src="./assets/insidemouth.png"
-          alt="Inside of The Grill Master's mouth"
-          style={{ opacity: contentFadeProgress, transition: "opacity 0.5s ease" }}
+          src="./assets/beergrill.png"
+          alt="Beer and Grill on top of it"
         />
-        <h1>ABOUT THE GRILL!</h1>
-        <p>Lorem ipsum dolores mama masii </p>
-        <p>Consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
-        <p>Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.</p>
-        <p>Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.</p>
-        <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+        <div className="rhs-content">
+          <EventMap />
+          <Rules />
+        </div>
       </div>
       <div className="content2">
         <img
