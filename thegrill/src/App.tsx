64a4da133b19f892ddbe2c106bef9bb317fa6f59
Miruna <mirunanego15@gmail.com>
2025-03-25 01:50:09 +0100

    add roof of mouth animation

--- thegrill/src/App.tsx
+++ thegrill/src/App.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import EventMap from './components/EventMap';
 import Rules from './components/Guidelines';
-import { motion, useScroll, useTransform } from "motion/react"
+import { motion, useScroll, useTransform, useSpring } from "motion/react"
 
 function App() {
 
@@ -16,32 +16,20 @@ function App() {
     return () => window.removeEventListener("resize", updateHeight);
   }, []);
 
-  const lipOffset = useTransform(scrollY, [0, viewportHeight], [0, -2 * viewportHeight])
+  const topLipOffset = useTransform(scrollY, [0, viewportHeight], [0, -0.5 * viewportHeight])
+  const bottomLipOffset = useTransform(scrollY, [0, viewportHeight], [0, -1.7 * viewportHeight])
   const lipOpacity = useTransform(scrollY, [0, 1.3 * viewportHeight], [1, 0])
+  const lipTransitionOpacity = useTransform(scrollY, [0, 0.5 * viewportHeight], [1, 0])
+  const roofOffset = useTransform(scrollY, [0, viewportHeight], [0, viewportHeight])
 
-  const lipThreshold = 500;
-  const contentFadeStart = 500;
-  const contentFadeDuration = 200;
-  const [scrollOffset, setScrollOffset] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollOffset(window.scrollY);
-    }
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-  const lipProgress = Math.min(scrollOffset / lipThreshold, 1);
-  const contentFadeProgress = Math.min(
-    Math.max((scrollOffset - contentFadeStart) / contentFadeDuration, 0),
-    1
-  );
+  const smoothedTopLipOffset = useSpring(topLipOffset, { stiffness: 170, damping: 23 });
+  const smoothedBottomLipOffset = useSpring(bottomLipOffset, { stiffness: 170, damping: 23 });
+  const smoothedRoofOffset = useSpring(roofOffset, { stiffness: 400, damping: 80 });
 
   return (
     <>
 
-      <div className="start-page" >
+      <div className="start-page" style={{ height: '250vh' }} >
         <motion.div
           className="mouth"
           style={{ position: 'absolute', width: '100%', height: '100vh', zIndex: 5 }}
@@ -50,24 +38,26 @@ function App() {
             className="upper"
             src="./assets/toplip.png"
             alt="Top lip of The Grill Master"
-            style={{ width: '100%', position: 'absolute', zIndex: 5, top: lipOffset, opacity: lipOpacity }}
+            style={{ width: '100%', position: 'absolute', zIndex: 5, top: smoothedTopLipOffset, opacity: lipOpacity }}
           />
           <motion.img
             className="lower"
             src="./assets/downlip.png"
             alt="Bottom lip of The Grill Master"
-            style={{ width: '100%', position: 'absolute', zIndex: 4, bottom: lipOffset, opacity: lipOpacity }}
+            style={{ width: '100%', position: 'absolute', zIndex: 4, bottom: smoothedBottomLipOffset, opacity: lipOpacity }}
           />
         </motion.div>
-        <div className="info-section" style={{ zIndex: 2, position: 'absolute', width: '100%', backgroundColor: 'blue' }}>
-          <div className="roof-of-mouth" style={{ position: 'absolute', width: '100%', top: '0vh' }}>
-            <img
-              src="./assets/insidemouth.png"
-              alt="Inside of The Grill Master's mouth"
-              style={{ position: 'absolute', width: '100%' }}
-            />
-          </div>
-        </div>
+        <motion.div
+          className="mouth-transition"
+          style={{ zIndex: 3, position: 'absolute', width: '100%', height: '100vh', opacity: lipTransitionOpacity, backgroundColor: 'black' }}
+        />
+        <motion.div className="info-section" style={{ zIndex: 2, position: 'absolute', width: '100%', top: smoothedRoofOffset }}>
+          <img
+            src="./assets/insidemouth.png"
+            alt="Inside of The Grill Master's mouth"
+            style={{ position: 'absolute', width: '100%' }}
+          />
+        </motion.div>
       </div >
 
 
