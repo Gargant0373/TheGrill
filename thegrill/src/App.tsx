0e615303d54683dc7d00a3b9f460ac95fd14b9f5
Miruna <mirunanego15@gmail.com>
2025-03-25 12:28:20 +0100

    more animation tweaks
    make it look like you are going inside the mouth, and also remove the
    fading of the lips

--- thegrill/src/App.tsx
+++ thegrill/src/App.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import EventMap from './components/EventMap';
 import Rules from './components/Guidelines';
-import { motion, useScroll, useTransform, useSpring, animate } from "motion/react"
+import { motion, useScroll, useTransform, useSpring } from "motion/react"
 
 function App() {
 
@@ -16,22 +16,37 @@ function App() {
     return () => window.removeEventListener("resize", updateHeight);
   }, []);
 
-  const topLipOffset = useTransform(scrollY, [0, viewportHeight], [0, -0.5 * viewportHeight])
-  const bottomLipOffset = useTransform(scrollY, [0, viewportHeight], [0, -1.7 * viewportHeight])
-  const lipOpacity = useTransform(scrollY, [0, 1.3 * viewportHeight], [1, 0])
+  // TODO this is very taxing for the cpu :') idk what to do abt it
+  const topLipOffset = useTransform(scrollY, [0, viewportHeight], [0, -0.01 * viewportHeight])
+  const bottomLipOffset = useTransform(scrollY, [0, viewportHeight], [0, -1.65 * viewportHeight])
+  //  const lipOpacity = useTransform(scrollY, [0, 1 * viewportHeight], [1, 1])
   const lipTransitionOpacity = useTransform(scrollY, [0, 0.5 * viewportHeight], [1, 0])
   const roofOffset = useTransform(scrollY, [0, viewportHeight], [0, viewportHeight])
-  const infoScale = useTransform(scrollY, [0, viewportHeight], [1, 1])
+  const infoScale = useTransform(scrollY, [0, viewportHeight], [0.9, 1])
+  const mouthScale = useTransform(scrollY, [0, viewportHeight], [1, 1.3])
 
-  const smoothedTopLipOffset = useSpring(topLipOffset, { stiffness: 170, damping: 23 });
-  const smoothedBottomLipOffset = useSpring(bottomLipOffset, { stiffness: 170, damping: 23 });
-  const smoothedRoofOffset = useSpring(roofOffset, { stiffness: 500, damping: 80 });
+  const smoothedTopLipOffset = useSpring(topLipOffset, { stiffness: 5, damping: 10 });
+  const smoothedBottomLipOffset = useSpring(bottomLipOffset, { stiffness: 100, damping: 20 });
+  const smoothedRoofOffset = useSpring(roofOffset, { stiffness: 800, damping: 100 });
+  const smoothedMouthScale = useSpring(mouthScale, { stiffness: 800, damping: 30 })
+  const smoothedInfoScale = useSpring(infoScale, { stiffness: 1000, damping: 50 })
 
-  // Function to scroll smoothly to 100vh
+  // Function to scroll smoothly to 100vh - chatGPT
   const scrollTo100vh = () => {
     //    animate(scrollY, 2 * viewportHeight, { type: "tween", duration: 0.8, ease: "easeInOut" });
     window.scrollTo({ top: viewportHeight, behavior: "smooth" });
   };
+
+  // Make mouth dissapear completely instead of fading, cause it makes the mouth opening look nicer
+  // TODO this does cause some visual glitches when scrolling really fast
+  const [isMouthVisible, setIsMouthVisible] = useState(true);
+  useEffect(() => {
+    const unsubscribe = scrollY.on("change", (latest) => {
+      setIsMouthVisible(latest < 1.1 * viewportHeight);
+    });
+
+    return () => unsubscribe();
+  }, [scrollY, viewportHeight]);
 
   return (
     <>
@@ -56,28 +71,33 @@ function App() {
         >
           Scroll to 100vh
         </motion.button>
-        <motion.div
-          className="mouth"
-          style={{ position: 'absolute', width: '100%', height: '100vh', zIndex: 5 }}
-        >
-          <motion.img
-            className="upper"
-            src="./assets/toplip.png"
-            alt="Top lip of The Grill Master"
-            style={{ width: '100%', position: 'absolute', zIndex: 5, top: smoothedTopLipOffset, opacity: lipOpacity }}
-          />
-          <motion.img
-            className="lower"
-            src="./assets/downlip.png"
-            alt="Bottom lip of The Grill Master"
-            style={{ width: '100%', position: 'absolute', zIndex: 4, bottom: smoothedBottomLipOffset, opacity: lipOpacity }}
-          />
-        </motion.div>
-        <motion.div
-          className="mouth-transition"
-          style={{ zIndex: 3, position: 'absolute', width: '100%', height: '100vh', opacity: lipTransitionOpacity, backgroundColor: 'black' }}
-        />
-        <motion.div className="info-section" style={{ zIndex: 1, position: 'absolute', width: '100%', top: smoothedRoofOffset, scale: infoScale }}>
+
+        {isMouthVisible && (
+          <>
+            <motion.div
+              className="mouth"
+              style={{ position: 'absolute', width: '100%', height: '100vh', zIndex: 5, scale: smoothedMouthScale }}
+            >
+              <motion.img
+                className="upper"
+                src="./assets/toplip.png"
+                alt="Top lip of The Grill Master"
+                style={{ width: '100%', position: 'absolute', zIndex: 5, top: smoothedTopLipOffset }}
+              />
+              <motion.img
+                className="lower"
+                src="./assets/downlip.png"
+                alt="Bottom lip of The Grill Master"
+                style={{ width: '100%', position: 'absolute', zIndex: 4, bottom: smoothedBottomLipOffset }}
+              />
+              <motion.div
+                className="mouth-transition"
+                style={{ zIndex: 3, position: 'absolute', width: '100%', height: '100vh', opacity: lipTransitionOpacity, backgroundColor: 'black' }}
+              />
+            </motion.div>
+          </>
+        )}
+        <motion.div className="info-section" style={{ zIndex: 1, position: 'absolute', width: '100%', top: smoothedRoofOffset, scale: smoothedInfoScale }}>
           <motion.img
             src="./assets/insidemouth.png"
             alt="Inside of The Grill Master's mouth"
@@ -108,16 +128,6 @@ function App() {
 
 
 
-      <div className="content2">
-        <img
-          src="./assets/beergrill.png"
-          alt="Beer and Grill on top of it"
-        />
-        <div className="rhs-content">
-          <EventMap />
-          <Rules />
-        </div>
-      </div>
       <div className="content2">
         <img
           src="./assets/beergrill.png"
