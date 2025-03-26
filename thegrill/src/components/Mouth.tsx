06e7bc329da8108a369e5d4e4026bd56ed1fa5d7
Miruna <mirunanego15@gmail.com>
2025-03-26 16:33:14 +0100

    fix mouth animation and separate css

--- thegrill/src/components/Mouth.tsx
+++ thegrill/src/components/Mouth.tsx
@@ -1,19 +1,16 @@
 import { motion, useTransform, useSpring, MotionValue } from "motion/react"
+import "./Mouth.css"
 
 function Mouth({ scrollY, viewportHeight }: { scrollY: MotionValue<number>, viewportHeight: number }) {
   // TODO this is very taxing for the cpu :') idk what to do abt it
-  const topLipOffset = useTransform(scrollY, [0, viewportHeight], [0, -0.01 * viewportHeight])
-  const bottomLipOffset = useTransform(scrollY, [0, viewportHeight], [0, -1.65 * viewportHeight]) //  const lipOpacity = useTransform(scrollY, [0, 1 * viewportHeight], [1, 1])
-  const lipTransitionOpacity = useTransform(scrollY, [0, 0.5 * viewportHeight], [0.8, 0])
-  const mouthScale = useTransform(scrollY, [0, viewportHeight], [1, 1.3])
+  const lipOffset = useTransform(scrollY, [0, viewportHeight], [0, -1 * viewportHeight])
+  const lipTransitionOpacity = useTransform(scrollY, [0, 0.5 * viewportHeight], [1, 0])
+  const mouthScale = useTransform(scrollY, [0, viewportHeight], [1, 2])
 
-  const smoothedTopLipOffset = useSpring(topLipOffset, { stiffness: 5, damping: 10 });
-  const smoothedBottomLipOffset = useSpring(bottomLipOffset, { stiffness: 100, damping: 20 });
-  const smoothedMouthScale = useSpring(mouthScale, { stiffness: 800, damping: 30 })
+  const smoothedLipOffset = useSpring(lipOffset, { stiffness: 150, damping: 30 });
 
   // Function to scroll smoothly to 100vh - chatGPT
   const scrollTo100vh = () => {
-    //    animate(scrollY, 2 * viewportHeight, { type: "tween", duration: 0.8, ease: "easeInOut" });
     window.scrollTo({ top: viewportHeight, behavior: "smooth" });
   };
 
@@ -43,23 +40,23 @@ function Mouth({ scrollY, viewportHeight }: { scrollY: MotionValue<number>, view
       <>
         <motion.div
           className="mouth"
-          style={{ position: 'absolute', width: '100%', height: '100vh', zIndex: 5, scale: smoothedMouthScale }}
+          style={{ zIndex: 5, scale: mouthScale }}
         >
           <motion.img
             className="upper"
             src="./assets/toplip.png"
             alt="Top lip of The Grill Master"
-            style={{ width: '100%', position: 'absolute', zIndex: 5, top: smoothedTopLipOffset }}
+            style={{ zIndex: 5, top: smoothedLipOffset }}
           />
           <motion.img
             className="lower"
             src="./assets/downlip.png"
             alt="Bottom lip of The Grill Master"
-            style={{ width: '100%', position: 'absolute', zIndex: 4, bottom: smoothedBottomLipOffset }}
+            style={{ zIndex: 4, bottom: smoothedLipOffset }}
           />
           <motion.div
             className="mouth-transition"
-            style={{ zIndex: 3, position: 'absolute', width: '100%', height: '100vh', opacity: lipTransitionOpacity, backgroundColor: 'black' }}
+            style={{ zIndex: 3, opacity: lipTransitionOpacity }}
           />
         </motion.div>
       </>
