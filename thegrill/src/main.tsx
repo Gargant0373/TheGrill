1aeb41e5e3aa270a38ea41f724d43034b5c19b61
Gargant <37416030+Gargant0373@users.noreply.github.com>
2025-04-09 16:45:49 +0200

    fix: ensure proper formatting in Mouth.css and main.tsx, add CNAME file for deployment

--- thegrill/src/main.tsx
+++ thegrill/src/main.tsx
@@ -1,23 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App.tsx'
-import Footer from './components/Footer.tsx'
 import Gallery from './Gallery'
+import './index.css'
+import Footer from './components/Footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
-          <>
-            <App />
-            <Footer />
-          </>
-        } />
+        <Route path="/" element={<App />} />
         <Route path="/gallery" element={<Gallery />} />
       </Routes>
     </BrowserRouter>
+    <Footer />
   </StrictMode>,
 )
