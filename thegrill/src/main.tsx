7f3f593d6c5917915ac4e7a951e7d802fdd8247a
Gargant <37416030+Gargant0373@users.noreply.github.com>
2025-04-16 16:35:05 +0200

    switched to hash router

--- thegrill/src/main.tsx
+++ thegrill/src/main.tsx
@@ -1,21 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App.tsx'
 import Footer from './components/Footer.tsx'
 import RedirectToForm from './components/RedirectToForm.tsx'
 import Gallery from './Gallery'
 import './index.css'
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/olympics" element={<RedirectToForm />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     <Footer />
   </StrictMode>,
-)
+);
