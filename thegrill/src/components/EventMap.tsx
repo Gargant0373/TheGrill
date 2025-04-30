1fbafdd762b77b3a430742d299d9c35d423640eb
Gargant <gargant0373@gmail.com>
2025-04-30 19:03:15 +0200

    location update

--- thegrill/src/components/EventMap.tsx
+++ thegrill/src/components/EventMap.tsx
@@ -5,8 +5,8 @@ function EventMap() {
         <div className="map">
             <h1>LOCATION</h1>
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d78565.40670640864!2d4.231897197265621!3d52.02201960000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5b531460f260b%3A0x2e7f2d9b4e705156!2sPark%20Delftse%20Hout!5e0!3m2!1sro!2snl!4v1742151280530!5m2!1sro!2snl"
                 width="100%"
+                src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d146.06180812122417!2d4.381849020458697!3d52.02005098552107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e6!4m0!4m3!3m2!1d52.02005820686467!2d4.381998553612656!5e1!3m2!1sro!2snl!4v1746032547216!5m2!1sro!2snl"
                 height="300em"
                 style={{ border: 0 }}
                 loading="lazy"
