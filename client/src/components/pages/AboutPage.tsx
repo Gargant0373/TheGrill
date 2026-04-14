814e13e3821f96a1106b03449a462ae86ebb6f77
mirunic15 <miruru2003@gmail.com>
2026-04-15 01:08:15 +0200

    change font

--- client/src/components/pages/AboutPage.tsx
+++ client/src/components/pages/AboutPage.tsx
@@ -1,7 +1,7 @@
 function AboutPage() {
   return (
     <div className="m-0 max-w-lg text-center space-y-4">
-      <h3 className="text-2xl font-title mb-4">Here's what you need to know!</h3>
+      <h3 className="text-2xl mb-4">Here's what you need to know!</h3>
       <p>On May 1st, we feast for the 5th time at Delftse Hout.</p>
       <p>Grill smoke will fill the air, beer will flow and fun will follow.</p>
       <p>The Grill is based on good vibes, no entrance fee required. But we cannot feed everyone! So bring your own grill, your own food, your own fire, for you and your friends.</p>
